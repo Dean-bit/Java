6721953a01a547fd4086fb111a526bc3e0987654
Dean-bit <65940289+Dean-bit@users.noreply.github.com>
2020-05-31 16:39:00 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,33 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        //Get all years divisible by 14 and 20
-        YearRange myFirstRange = new YearRange(1314, 2014);
-        myFirstRange.setFirstDivisor(14);
-        myFirstRange.setSecondDivisor(20);
-        myFirstRange.setRangeType(1);
-
-        //Get all Leap Years
-        YearRange mySecondRange = new YearRange(1100, 3150);
-        mySecondRange.setRangeType(2);
-
-        //Get all Olympic years divisible by 3
-        YearRange myThirdRange = new YearRange(1500, 1890);
-        myThirdRange.setFirstDivisor(3);
-        myThirdRange.setRangeType(3);
-
-        System.out.println("List all years divisible by "+
-                            myFirstRange.getFirstDivisor()+" and "+
-                            myFirstRange.getSecondDivisor());
-                            myFirstRange.printYears();
-        
-        System.out.println("List all leap years between "+
-                            mySecondRange.getFirstYear()+" and "+
-                            mySecondRange.getSecondYear());
-                            mySecondRange.printYears();
-                            
-        System.out.println("List of all Olympic years between "+
-                            myThirdRange.getFirstYear()+ " and "+
-                            myThirdRange.getSecondYear()+
-                            " that are divisible by "+ 
-                            myThirdRange.getFirstDivisor());
-                            myThirdRange.printYears();
+        System.out.println("-----------Start of Report-------------");
+        //Report of the First Building
+        Building myFirstBuilding = new Building();
+        myFirstBuilding.setNameBuilding("Skyline");
+        myFirstBuilding.setColour("Green");
+        myFirstBuilding.setNumFloors(16);
+        myFirstBuilding.setNumRoomsFloor(4);
+        myFirstBuilding.printBuilding();
+		 myFirstBuilding.setNameBuilding("Resort");
+        myFirstBuilding.setColour("purple");
+        myFirstBuilding.setNumFloors(16);
+        myFirstBuilding.setNumRoomsFloor(4);
+        myFirstBuilding.printBuilding();
+		 myFirstBuilding.setNameBuilding("Innerfort");
+        myFirstBuilding.setColour("pink");
+        myFirstBuilding.setNumFloors(16);
+        myFirstBuilding.setNumRoomsFloor(4);
+        myFirstBuilding.printBuilding();
+		myFirstBuilding.setNameBuilding("Comfort");
+        myFirstBuilding.setColour("Blue");
+        myFirstBuilding.setNumFloors(10);
+        myFirstBuilding.setNumRoomsFloor(9);
+        myFirstBuilding.printBuilding();
+		 myFirstBuilding.setNameBuilding("Towers");
+        myFirstBuilding.setColour("Green");
+        myFirstBuilding.setNumFloors(16);
+        myFirstBuilding.setNumRoomsFloor(4);
+        myFirstBuilding.printBuilding();
+        System.out.println("------------End of Report-------------");
     }
-	 
 }
